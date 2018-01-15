1b1622a4da0859a2835cd967d464deccc97609e5
joeypiccola <joey.piccola@gmail.com>
2018-01-14 22:11:24 -0700

    added init

---
+++ manifests/init.pp
@@ -0,0 +1,40 @@
+# == Class: puppet_win
+#
+#
+#
+#
+# === Parameters
+#
+# [*value*]
+#   The timezone to use. For a full list of available timezone run tzutil /l.
+#   Use the listed time zone ID (e.g. 'Eastern Standard Time')
+#
+# === Examples
+#
+#  class { ::puppet_win:
+#    timezone = 'Mountain Standard Time',
+#  }
+#
+# === Authors
+#
+# Joey Piccola <joey@joeypiccola.com>
+#
+# === Copyright
+#
+# Copyright (C) 2016 Joey Piccola.
+#
+class puppet_win (
+
+  String $value = undef,
+
+){
+
+  # parameter validation
+  validate_string($value)
+
+  exec { 'run_powershell':
+    command  => "write-output '${timezone}'",
+    provider => powershell,
+  }
+
+}
